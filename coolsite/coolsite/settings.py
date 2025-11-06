42473f98ddf6db770002e251964966f46b3fc45d
Artem Cvetkoff <artem.flowers1@gmail.com>
2025-11-06 13:52:04 +0500

    Поместил проект в Docker

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -84,12 +84,25 @@ WSGI_APPLICATION = 'coolsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if os.environ.get('DOCKER_ENV'):  # если запущено в Docker
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'vibemusic'),
+            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+            'PORT': '5432',
+        }
     }
-}
+else:  # локальная разработка
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
